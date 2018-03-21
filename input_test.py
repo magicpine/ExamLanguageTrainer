fd412945aa390c06ea287db84ceb25d63c0b4219
magicpine <c00223037@itcarlow.ie>
2018-03-21 16:18:42 +0000

    Intital push to server

--- input_test.py
+++ input_test.py
@@ -5,15 +5,19 @@ def getText(filename):
     sys = 'soffice --headless --convert-to txt:Text ' + filename
     os.system(sys)
     new_filename = filename.split('.')[0] + '.txt'
-    print (new_filename)
+    #print (new_filename)
     with open(new_filename, 'r') as myfile:
         data=myfile.read().replace('\n', '')
     return data
 
 if '__main__' == __name__:
-    example = getText('ass3-2018.doc')
-    print (example) # str
+    example = getText('tmp/ass3-2018.doc')
+    #print (example) # str
     example  = example.replace('\t', '')
-    
     exampleList = example.split(' ')
+    cleanList = []
+    for word in exampleList:
+        if word.startswith("\\"):
+            print(word)
+            cleanList.append(word)
     print(exampleList)
